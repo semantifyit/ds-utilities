fbf10245ff3009d95df22c32b14caf9d110b590a
omar <toshiro_umezawa99@yahoo.de>
2021-09-14 14:46:55 +0200

    Changed License. Added file definition for npm.

--- examples/example-node.js
+++ examples/example-node.js
@@ -1,6 +1,6 @@
-const DsUtilities = require('../src');
+const DsUtilities = require("../src");
 
 // const myDsUtilitiesV5 = DsUtilities.getDsUtilitiesForDsSpecVersion("5.0");
 const myDsUtilitiesV7 = DsUtilities.getDsUtilitiesForDsSpecVersion("7.0");
-//console.log(myDsUtilitiesV5.getDsStandardContext()); todo
+// console.log(myDsUtilitiesV5.getDsStandardContext()); todo
 console.log(myDsUtilitiesV7.getDsStandardContext());
